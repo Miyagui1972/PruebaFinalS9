a2009bfde1f096f2764e4d49bf7d7a2a2b01a64e
Jose Vergara Veas <jose.antonio.vergara.veas@gmail.com>
2024-10-04 19:09:02 -0300

    Actualización de archivos BD
    Se realizaron procesos de borrado de codigo innecesario .

--- DataBase/store.sql
+++ DataBase/store.sql
@@ -1,26 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 21-03-2018 a las 02:19:49
--- Versión del servidor: 10.1.24-MariaDB
--- Versión de PHP: 7.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `store`
---
 
 -- --------------------------------------------------------
 
